68413fe26b3a2ddb58210de42a53c4e60b790ec1
Douglas08Santos <santosdouglas0808@gmail.com>
2019-08-12 14:27:59 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fragment/FragmentRight.kt
@@ -0,0 +1,14 @@
+package com.example.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentRight: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_layout_right, container, false)
+    }
+}
